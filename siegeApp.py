b3d8b02c90a9149a7b839ec7e42bb8bca102f61f
conpri <conpri806@gmail.com>
2026-03-19 12:47:59 -0400

    Updated best.pt for better model

--- siegeApp.py
+++ siegeApp.py
@@ -2,7 +2,6 @@ import cv2
 from ultralytics import YOLO
 import torch
 
-#Checks for GPU
 print("HIP available:", torch.cuda.is_available())
 print("Device count:", torch.cuda.device_count())
 print("HIP version:", torch.version.hip)
@@ -11,12 +10,12 @@ print("Device name:", torch.cuda.get_device_name(0) if torch.cuda.is_available()
 #Just incase of glitch
 cv2.destroyAllWindows()
 
-modelPath = "/mnt/c/Users/conpr.CONNORSPC/Documents/Siege/Content/train_copy/weights/best.pt" #WSL version
-#modelPath= "C:\\Users\\conpr.CONNORSPC\\Documents\\Siege\\content\\train_copy\\weights\\best.pt" # Windows Version
+#modelPath = "/mnt/c/Users/conpr.CONNORSPC/Documents/Siege/model2/Content/train_copy/weights/best.pt" #WSL version
+modelPath= "C:\\Users\\conpr.CONNORSPC\\Documents\\Siege\\model2\\content\\train_copy\\weights\\best.pt" # Windows Version
 model = YOLO(modelPath)
 
-video_path = "/mnt/c/Users/conpr.CONNORSPC/Documents/Siege/Video.mp4" #WSL version
-#video_path = "C:\\Users\\conpr.CONNORSPC\\Documents\\Siege\\Video.mp4" #Windows Version
+#video_path = "/mnt/c/Users/conpr.CONNORSPC/Documents/Siege/Video.mp4" #WSL version
+video_path = "C:\\Users\\conpr.CONNORSPC\\Documents\\Siege\\Video.mp4" #Windows Version
 cap = cv2.VideoCapture(video_path)
 
 if not cap.isOpened():
@@ -34,13 +33,14 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
 
-vidURL = "/mnt/c/Users/conpr.CONNORSPC/Documents/Siege/output_video_with_boxes.mp4" #WSL version
-#vidURL = "C:\\Users\\conpr.CONNORSPC\\Documents\\Siege\\output_video_with_boxes.mp4" #Windows version
+#vidURL = "/mnt/c/Users/conpr.CONNORSPC/Documents/Siege/output_video_with_boxes.mp4" #WSL version
+vidURL = "C:\\Users\\conpr.CONNORSPC\\Documents\\Siege\\output_video_with_boxes.mp4" #Windows version
 out = cv2.VideoWriter(vidURL, fourcc, fps, (width, height))
 
 #Stuff to be able to resize window
-cv2.namedWindow("YOLO Siege Tracker", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("YOLO Siege Tracker", width, height)
+cv2.namedWindow("YOLO Operator Tracker", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("YOLO Operator Tracker", width, height)
+
 # Video goes frame by frame to check for operators
 while cap.isOpened():
     ret, frame = cap.read()
@@ -54,16 +54,16 @@ while cap.isOpened():
     annotated_frame = results[0].plot()
 
     # Display the frame with bounding boxes
-    cv2.imshow("YOLO Siege Tracker", annotated_frame)
+    cv2.imshow("YOLO Operator Tracker", annotated_frame)
 
-    # Optional: Write the frame to the output video
+    #Write the frame to the output video
     out.write(annotated_frame)
 
-    # Breaks loop if 'q' is pressed
+    # Break the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release resources so things dont break
+# Release resources so things don't break
 cap.release()
 out.release()
 cv2.destroyAllWindows()
